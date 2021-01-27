a7c1f070c47516fb122840e140b7f1d32889ad49
HLAM <hemilioaraujo@gmail.com>
2021-01-27 18:55:39 -0300

    setting staticfiles/ mediafiles/ to settings.py

--- portal_ensino/settings.py
+++ portal_ensino/settings.py
@@ -122,4 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+# STATIC & MEDIA -  AMBIENTE DE DESENVOLVIMENTO
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'mediafiles'
