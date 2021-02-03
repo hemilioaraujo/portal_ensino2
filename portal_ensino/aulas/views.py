026458c68e713272a83437477fb3ad2c6d30b7d4
HLAM <hemilioaraujo@gmail.com>
2021-02-02 21:58:46 -0300

    added aulas template

--- portal_ensino/aulas/views.py
+++ portal_ensino/aulas/views.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from portal_ensino.comentarios.models import Comentarios
+
 
 # Create your views here.
+@login_required
+def aula(request):
+    comentarios = Comentarios.objects.filter(aula_referente=request.user.aula_atual.id).order_by('data')
+    return render(request, 'aulas/aula.html', {'usuario': request.user, 'comentarios': comentarios})
