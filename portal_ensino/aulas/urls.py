52df08f4c4a00a2004a8382a8e1d39acfcc19d86
hemilioaraujo <hemilioaraujo@gmail.com>
2021-02-08 13:35:19 -0300

    added previous function to aulas view

--- portal_ensino/aulas/urls.py
+++ portal_ensino/aulas/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
-from portal_ensino.aulas.views import aula, proxima_aula
+from portal_ensino.aulas.views import aula, proxima_aula, aula_anterior
 from portal_ensino.base.views import xpto
 
 urlpatterns = [
     # XPTO
     # path('proxima_aula/', xpto, name='proxima_aula'),
-    path('aula_anterior/', xpto, name='aula_anterior'),
+    # path('aula_anterior/', xpto, name='aula_anterior'),
     path('deletar-comentario/<int:id>', xpto, name="deletar_comentario"),
 
     path('', aula, name='aula'),
     path('proxima_aula/', proxima_aula, name='proxima_aula'),
+    path('aula_anterior/', aula_anterior, name='aula_anterior'),
 ]
