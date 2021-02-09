8281d5a5cc6552c0bff8fab7ba3be22558ee45d7
hemilioaraujo <hemilioaraujo@gmail.com>
2021-02-09 11:15:03 -0300

    removed Aulas responsibility over Comentarios posting comments

--- portal_ensino/aulas/views.py
+++ portal_ensino/aulas/views.py
@@ -4,34 +4,14 @@ from django.shortcuts import render, redirect
 from portal_ensino.aulas.models import Aulas
 from portal_ensino.base.models import User
 from portal_ensino.comentarios.models import Comentarios
+from portal_ensino.comentarios.views import post_comentario
 
-
-# # Create your views here.
-# @login_required
-# def aula(request):
-#     comentarios = Comentarios.objects.filter(aula_referente=request.user.aula_atual.id).order_by('data')
-#     return render(request, 'aulas/aula.html', {'usuario': request.user, 'comentarios': comentarios})
 
 @login_required
 def aula(request):
     if request.method == 'POST':
         if 'enviar' in request.POST:
-            resposta = request.POST
-            comentario = resposta['comentario']
-            usuario = User.objects.get(id=request.user.id)
-            aula_referente = Aulas.objects.get(id=request.user.aula_atual.id)
-
-            try:
-                Comentarios.objects.create(
-                    comentario=comentario,
-                    aula_referente=aula_referente,
-                    user=usuario
-                )
-            except NameError:
-                print('erro ao postar comentário')
-
-            return redirect('base:aula')
-        else:
+            post_comentario(request)
             return redirect('base:aula')
     else:
         comentarios = Comentarios.objects.filter(aula_referente=request.user.aula_atual.id).order_by('data')
--- portal_ensino/comentarios/views.py
+++ portal_ensino/comentarios/views.py
@@ -1,3 +1,20 @@
-# from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from portal_ensino.aulas.models import Aulas
+from portal_ensino.base.models import User
+from portal_ensino.comentarios.models import Comentarios
 
-# Create your views here.
+def post_comentario(request):
+    resposta = request.POST
+    comentario = resposta['comentario']
+    usuario = User.objects.get(id=request.user.id)
+    aula_referente = Aulas.objects.get(id=request.user.aula_atual.id)
+
+    try:
+        Comentarios.objects.create(
+            comentario=comentario,
+            aula_referente=aula_referente,
+            user=usuario
+        )
+    except NameError:
+        print('erro ao postar comentário')
