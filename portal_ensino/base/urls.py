caa8a15903cbad1eb28b8154653e74eb4791f276
hemilioaraujo <hemilioaraujo@gmail.com>
2021-02-10 12:51:12 -0300

    added profile template

--- portal_ensino/base/urls.py
+++ portal_ensino/base/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from portal_ensino.base.views import home, xpto
+from portal_ensino.base.views import home, xpto, exibir_profile
 from portal_ensino.api import urls as urls_api
 from portal_ensino.aulas import urls as urls_aulas
 from portal_ensino.questoes import urls as urls_questoes
@@ -9,10 +9,10 @@ app_name = 'base'
 
 urlpatterns = [
     path('', home, name='home'),
+    path('profile/', exibir_profile, name='exibir_profile'),
     path('', xpto, name='atualizar_usuario'),
     path('', xpto, name='novo_usuario'),
     path('', xpto, name='password_reset'),
-    path('', xpto, name='exibir_profile'),
     path('', xpto, name='sobre'),
 
     # LOGIN
--- portal_ensino/base/views.py
+++ portal_ensino/base/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 def home(request):
     return render(request, 'base/home.html')
+
+
+@login_required
+def exibir_profile(request):
+    return render(request, 'user/profile_usuario.html', {'user': request.user})
 
 
 def xpto(request):
